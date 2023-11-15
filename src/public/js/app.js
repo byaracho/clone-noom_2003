22b9a150281022eedd0e6ffec7eb5057c20ba16e
ara <byaracho@gmail.com>
2023-11-15 14:55:46 +0900

    add: 소켓끼리 메시지 확인 테스트

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -5,13 +8,20 @@ socket.addEventListener("open", () => {
 })
 
 socket.addEventListener("message", (message) => {
-  console.log("Just got this:", message.data, "from the server");
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
   console.log("Disconnected form Server");
 })
 
-setTimeout(() => {
-  socket.send("hello from browser!");
-}, 5000); // 브라우저에서 서버로 메시지 전달
+function handleSubmit(event) {
+  event.preventDefault(); // 이벤트를 취소하는 메서드, 이벤트가 제공하는 원래 기능을 사용하지 않고자 할 때 사용
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
