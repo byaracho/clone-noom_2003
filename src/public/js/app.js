908b43b087274825aa01dd1919394098484e2e9b
ara <byaracho@gmail.com>
2023-11-15 16:37:04 +0900

    add: 닉네임 핸들러

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,13 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload }
+  return JSON.stringify(msg)
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
@@ -20,8 +26,17 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault(); // 이벤트를 취소하는 메서드, 이벤트가 제공하는 원래 기능을 사용하지 않고자 할 때 사용
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value)) // 브라우저에서 JSON 객체를 문자열로 변환해서 서버로 전달
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+
 messageForm.addEventListener("submit", handleSubmit);
+messageForm.addEventListener("submit", handleNickSubmit);
